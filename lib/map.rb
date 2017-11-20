eaeaae362694edc0a10fa72c088355688aea8d91
ghostofrasputin <jalprest@ucsc.edu>
2017-11-19 16:21:56 -0800

    - added floor map procedural generation

--- lib/map.rb
+++ lib/map.rb
@@ -24,14 +24,98 @@ class Map
   end
 
   def generate_floor(rooms)
-    map.each do |row|
-      row.each do |col|
+    startx = 0
+    starty = 0
+    goalx = 0
+    goaly = 0
+    if rand < 0.5
+      # top or bottom area
+      startx = rand < 0.5 ? 1 : 3
+      starty = rand < 0.5 ? 0 : 4
+      goalx = rand < 0.5 ? 1 : 3
+      goaly = starty == 4 ? 0 : 4
+    else
+      # left or right area
+      startx = rand < 0.5 ? 0 : 4
+      starty = rand < 0.5 ? 1 : 3
+      goalx = startx == 4 ? 0 : 4
+      goaly = rand < 0.5 ? 1 : 3
+    end
 
+    # fill map with random walls
+    map.each_with_index do |row, i|
+      row.each_with_index do |col , j|
+        if not (i == goalx and j == goaly)
+          if rand < 0.3
+            map[i][j] = 'W'
+          end
+        end
+      end
+    end
+
+    # add room layout to map
+    nodes = bfs([startx,starty], [goalx,goaly])
+    nodes.each do |node|
+      i = node[0]
+      j = node[1]
+      map[i][j] = random_room(rooms)
+    end
+
+    print_map
+
+  end
+
+  def random_room(rooms)
+    while true
+      r = rooms.sample
+      if not r.label == "boss"
+        rooms.delete(r)
+        return r
       end
     end
   end
 
-  def bfs
+  def successor(node)
+    successors = []
+    i = node[0]
+    j = node[1]
+    if i != 0
+      successors << [i-1,j]
+    end
+    if j != 0
+      successors << [i,j-1]
+    end
+    if i != 4
+      successors << [i+1,j]
+    end
+    if j != 4
+      successors << [i,j+1]
+    end
+    return successors.delete_if {|n| map[n[0]][n[1]] == 'W'}
+  end
+
+  def bfs(start, goal)
+    set = []
+    queue = []
+    queue.unshift([start, []])
+    while queue.length>0
+      current = queue.pop
+      node = current[0]
+      path = current[1]
+      if node[0] == goal[0] and node[1] == goal[1]
+        return path << node
+      end
+      successor(node).each do |s|
+        if not set.include? s
+          new_path = [].replace(path) << node
+          state = [s,new_path]
+          set << s
+          queue.unshift(state)
+        end
+      end
+    end
+    puts "no path"
+    return []
   end
 
   # parses a Yaml file to list of room objects
@@ -40,8 +124,8 @@ class Map
     count = 1
     rooms = []
     yml_array.each do |table|
+      room = Room.new
       table[count].each do |key, val|
-        room = Room.new
         case key
         when "label"
           room.label = val
@@ -65,11 +149,28 @@ class Map
           room.load_image(val)
         else
         end
-        rooms << room
       end
+      rooms << room
       count+=1
     end
     return rooms
+  end
+
+  # print out map layout
+  def print_map
+    map.each_with_index do |row, i|
+      row.each_with_index do |col , j|
+        #puts col
+        if col.is_a? Room
+          print 'R'
+        elsif col == 'W'
+          print 'W'
+        else
+          print '#'
+        end
+      end
+      puts ''
+    end
   end
 
 end
